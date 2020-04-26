de54907569ba8688fe20acf94db4a00060b83074
Le Yang <36768174+Easonrust@users.noreply.github.com>
2020-04-26 21:56:30 +0800

    let it move

--- homework4/fifteen.js
+++ homework4/fifteen.js
@@ -1,5 +1,6 @@
 "use strict";
-
+var blankTop=300;
+var blankLeft=300;
 window.onload = function () {
     var puzzlearea = document.getElementById("puzzlearea");
     for (var i = 0; i < 15; i++) {
@@ -8,7 +9,20 @@ window.onload = function () {
         tile.className="tile";
         tile.id="tile"+num;
         tile.innerHTML=num;
+        tile.onclick=move;
         puzzlearea.appendChild(tile);
         this.console.log(tile.parentNode.id);   
     }
+}
+function move(){
+    var top=this.offsetTop;
+    var left=this.offsetLeft;
+    if((top===blankTop)&&((left===blankLeft-100)||(left===blankLeft+100))){
+		this.style.left= blankLeft+"px";
+		blankLeft= left;
+	}
+	else if((left===blankLeft)&&((top===blankTop-100)||(top===blankTop+100))){
+		this.style.top= blankTop+"px";
+		blankTop=top;
+	}
 }
