ce089277792f2f3d572f15e926bce7f25cbfab14
Le Yang <36768174+Easonrust@users.noreply.github.com>
2020-06-16 01:15:59 +0800

    long press to edit

--- project/TodoMVC.js
+++ project/TodoMVC.js
@@ -58,25 +58,84 @@ function createItem(message) {
     item.appendChild(itemRemove);
 
 
-    item.addEventListener('click', function () {
-        let id = item.id.split('_')[1];
 
-        if (item.classList.contains(CL_COMPLETED)) {
-            item.classList.remove(CL_COMPLETED);
-            model.data.items[id].state = CL_ACTIVE;
-        } else {
-            item.classList.remove(CL_ACTIVE);
-            model.data.items[id].state = CL_COMPLETED;
+
+    item.addEventListener("touchstart", function (e) {
+        console.log('touchstart');
+        timer = setTimeout(function () {
+            console.log('LongPress');
+            var editBox = document.createElement('input');
+            var finished = false;
+            editBox.setAttribute('class', 'item-content');
+            editBox.setAttribute('type', 'text');
+            editBox.setAttribute('value', itemContent.innerHTML);
+
+            function finish() {
+                if (finished) return;
+                finished = true;
+                item.removeChild(editBox);
+            }
+
+            editBox.addEventListener('blur', function () {
+                finish();
+            });
+
+            editBox.addEventListener('keyup', function (ev) {
+                if (ev.keyCode == 13) {
+                    itemContent.innerHTML = this.value;
+                    var id = item.id.split('_')[1];
+                    console.log('id:' + id);
+                    model.data.items[id].msg = this.value;
+                    model.flush();
+                    update();
+                    finish();
+                }
+            });
+            editBox.addEventListener('touchend', function (ev) {
+
+                //阻止事件冒泡
+                ev.stopPropagation()
+            })
+
+            item.appendChild(editBox);
+            editBox.focus();
+            e.preventDefault();
+        }, 800);
+    });
+    item.addEventListener("touchmove", function (e) {
+        console.log('touchmove');
+        clearTimeout(timer);
+        timer = 0;
+    });
+    item.addEventListener("touchend", function (e) {
+        console.log('touchend');
+        clearTimeout(timer);
+        if (timer != 0) {
+            let id = item.id.split('_')[1];
+
+            if (item.classList.contains(CL_COMPLETED)) {
+                item.classList.remove(CL_COMPLETED);
+                model.data.items[id].state = CL_ACTIVE;
+            } else {
+                item.classList.remove(CL_ACTIVE);
+                model.data.items[id].state = CL_COMPLETED;
+            }
+            model.flush();
+            update();
         }
-        model.flush();
-        update();
-    })
+        return false;
+    });
 
-    itemRemove.addEventListener('click', function () {
+
+
+    itemRemove.addEventListener('touchend', function (ev) {
         let id = item.id.split('_')[1];
         model.data.items.splice(id, 1);
         model.flush();
         update();
+
+        //阻止事件冒泡
+        ev.stopPropagation()
     })
 
     return item;
@@ -143,16 +202,16 @@ window.onload = function () {
         addNewItem();
     }, )
 
-    $('#add').addEventListener('click', addNewItem);
+    $('#add').addEventListener('touchend', addNewItem);
 
-    $('.clear-completed').addEventListener('click', clearCompletedItems);
+    $('.clear-completed').addEventListener('touchend', clearCompletedItems);
 
-    $('.toggle-all').addEventListener('click', selectAllItems);
+    $('.toggle-all').addEventListener('touchend', selectAllItems);
 
     let filters = $All('.filters li a');
     for (let i = 0; i < filters.length; ++i) {
         (function (filter) {
-            filter.addEventListener('click', function () {
+            filter.addEventListener('touchend', function () {
                 for (let j = 0; j < filters.length; ++j) {
                     filters[j].classList.remove(CL_SELECTED);
                 }
