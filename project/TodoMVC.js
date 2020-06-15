30a4018e8f67d17872fa1a0eb33af07abd3e031d
Le Yang <36768174+Easonrust@users.noreply.github.com>
2020-06-15 23:48:37 +0800

    main logic

---
+++ project/TodoMVC.js
@@ -0,0 +1,167 @@
+// model for localstorage
+var model = window.model;
+// Set some const status string
+const CL_COMPLETED = 'Completed';
+const CL_SELECTED = 'selected';
+const CL_ACTIVE = 'Active';
+
+// Search helper
+var $ = function (sel) {
+    return document.querySelector(sel);
+};
+var $All = function (sel) {
+    return document.querySelectorAll(sel);
+};
+// Update the list
+function update() {
+    let filter = model.data.filter;
+    let items = model.data.items;
+    let list = $('#todo-list');
+    let active = $('.todo-count');
+
+    // Clear the list
+    list.innerHTML = '';
+
+    for (let i = 0; i < items.length; ++i) {
+        let item = items[i];
+        if (item.state == filter || filter == 'All') {
+            let tempItem = createItem(item.msg);
+            tempItem.id = 'item_' + i;
+            tempItem.classList.add(item.state);
+            //console.log(tempItem.id);
+            list.insertBefore(tempItem, list.firstChild);
+        }
+    }
+    //console.log('items.length:'+items.length);
+    console.log('list.children.length:' + list.children.length);
+
+    active.innerHTML = list.children.length + ' items left';
+}
+
+//创建todo条目
+function createItem(message) {
+    let item = document.createElement('div');
+    item.classList.add('list-item');
+
+
+
+    let itemContent = document.createElement('div');
+    itemContent.classList.add('item-content');
+    itemContent.innerHTML = message;
+
+    let itemRemove = document.createElement('div');
+    itemRemove.classList.add('item-remove');
+    itemRemove.innerHTML = '&#10005';
+
+
+    item.appendChild(itemContent);
+    item.appendChild(itemRemove);
+
+
+    item.addEventListener('click', function () {
+        let id = item.id.split('_')[1];
+
+        if (item.classList.contains(CL_COMPLETED)) {
+            item.classList.remove(CL_COMPLETED);
+            model.data.items[id].state = CL_ACTIVE;
+        } else {
+            item.classList.remove(CL_ACTIVE);
+            model.data.items[id].state = CL_COMPLETED;
+        }
+        model.flush();
+        update();
+    })
+
+    itemRemove.addEventListener('click', function () {
+        let id = item.id.split('_')[1];
+        model.data.items.splice(id, 1);
+        model.flush();
+        update();
+    })
+
+    return item;
+}
+
+function addNewItem() {
+    let content = $('#input-content');
+    let message = content.value;
+    if (message == '') {
+        console.warn('message is empty');
+        return;
+    }
+    let newItem = {
+        msg: message,
+        state: CL_ACTIVE
+    };
+    console.log('message:' + message);
+    model.data.items.push(newItem);
+    model.flush();
+    update();
+    content.value = '';
+}
+
+function clearCompletedItems() {
+    let items = model.data.items;
+    for (let i = items.length - 1; i >= 0; --i) {
+        let temp = items[i];
+        console.log(i);
+        if (temp.state == CL_COMPLETED) {
+            console.log('Delete item ' + i);
+            items.splice(i, 1)
+        }
+    }
+    model.flush();
+    update();
+}
+
+function selectAllItems() {
+    let items = model.data.items;
+    let toggleAll = $('.toggle-all');
+    let newState;
+
+    //console.log('select all');
+    if (toggleAll.classList.contains(CL_SELECTED)) {
+        toggleAll.classList.remove(CL_SELECTED)
+        newState = CL_ACTIVE;
+    } else {
+        //console.log('unselected');
+        toggleAll.classList.add(CL_SELECTED);
+        newState = CL_COMPLETED;
+    }
+    for (let i = 0; i < items.length; ++i) {
+        items[i].state = newState;
+    }
+    model.flush();
+    update();
+}
+
+window.onload = function () {
+    model.init(update);
+
+    $('#input-content').addEventListener('keyup', function (ev) {
+        if (ev.keyCode != 13) return;
+        addNewItem();
+    }, )
+
+    $('#add').addEventListener('click', addNewItem);
+
+    $('.clear-completed').addEventListener('click', clearCompletedItems);
+
+    $('.toggle-all').addEventListener('click', selectAllItems);
+
+    let filters = $All('.filters li a');
+    for (let i = 0; i < filters.length; ++i) {
+        (function (filter) {
+            filter.addEventListener('click', function () {
+                for (let j = 0; j < filters.length; ++j) {
+                    filters[j].classList.remove(CL_SELECTED);
+                }
+                console.log('filter:' + filter.innerHTML);
+                filter.classList.add(CL_SELECTED);
+                model.data.filter = filter.innerHTML;
+                model.flush();
+                update();
+            });
+        })(filters[i])
+    }
+}
